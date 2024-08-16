a56fcdb36f85cb73cbe6a0d8a1fabdf0c711091d
saurabhaloneai <saurabhaloney85@gmail.com>
2024-08-16 23:03:22 +0530

    model start

--- model.py
+++ model.py
@@ -17,4 +17,21 @@ class EncoderCNN(nn.Module):
         features=features.permute(0,2,3,1)
         features=features.view(features.size(0),-1,features.size(-1))
         return features #(batch_size,49,2048)
+
+class BahadnauAttention(nn.Module):
+    def __init__(self):
+        super(BahadnauAttention,self).__init__()
+        self.U=nn.Linear(512,512)
+        self.W=nn.Linear(2048,512)
+        self.v=nn.Linear(512,1)
+        self.tanh=nn.Tanh()
+        self.softmax=nn.Softmax(1)
         
+    def forward(self,features,hidden_states):
+        U_hidden=self.U(hidden_states)#(batch_size,512)
+        W_features=self.W(features)#(batch_size,49,512)
+        attention=self.tanh(U_hidden.unsqueeze(1)+W_features)
+        e=self.v(attention).squeeze(2) #(batch_size,1)
+        alpha=self.softmax(e)
+        context=(features*alpha.unsqueeze(2)).sum(1)
+        return alpha,context
