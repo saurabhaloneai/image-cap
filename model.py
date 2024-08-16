84e1e7e940c779960809e5c50fa6c2be6c748e40
saurabhaloneai <saurabhaloney85@gmail.com>
2024-08-16 23:03:49 +0530

    model added

--- model.py
+++ model.py
@@ -35,3 +35,109 @@ class BahadnauAttention(nn.Module):
         alpha=self.softmax(e)
         context=(features*alpha.unsqueeze(2)).sum(1)
         return alpha,context
+    
+    
+class DecoderRNN(nn.Module):
+    def __init__(self,embed_size, vocab_size, attention_dim,encoder_dim,decoder_dim,drop_prob=0.3):
+        super().__init__()
+        
+        #save the model param
+        self.vocab_size = vocab_size
+        self.attention_dim = attention_dim
+        self.decoder_dim = decoder_dim
+        
+        self.embedding = nn.Embedding(vocab_size,embed_size)
+        self.attention = BahadnauAttention()
+        
+        
+        self.init_h = nn.Linear(encoder_dim, decoder_dim)  
+        self.init_c = nn.Linear(encoder_dim, decoder_dim)  
+        self.lstm_cell = nn.LSTMCell(embed_size+encoder_dim,decoder_dim,bias=True)
+        self.f_beta = nn.Linear(decoder_dim, encoder_dim)
+        
+        
+        self.fcn = nn.Linear(decoder_dim,vocab_size)
+        self.drop = nn.Dropout(drop_prob)
+        
+        
+    
+    def forward(self, features, captions):
+        
+        #vectorize the caption
+        embeds = self.embedding(captions)
+        
+        # Initialize LSTM state
+        h, c = self.init_hidden_state(features)  # (batch_size, decoder_dim)
+        
+        #get the seq length to iterate
+        seq_length = len(captions[0])-1 #Exclude the last one
+        batch_size = captions.size(0)
+        num_features = features.size(1)
+        
+        preds = torch.zeros(batch_size, seq_length, self.vocab_size).to(device)
+        alphas = torch.zeros(batch_size, seq_length,num_features).to(device)
+                
+        for s in range(seq_length):
+            alpha,context = self.attention(features, h)
+            lstm_input = torch.cat((embeds[:, s], context), dim=1)
+            h, c = self.lstm_cell(lstm_input, (h, c))
+                    
+            output = self.fcn(self.drop(h))
+            
+            preds[:,s] = output
+            alphas[:,s] = alpha  
+        
+        
+        return preds, alphas
+    
+    def generate_caption(self,features,max_len=20,vocab=None):
+        # Inference part
+        # Given the image features generate the captions
+        
+        batch_size = features.size(0)
+        h, c = self.init_hidden_state(features)  # (batch_size, decoder_dim)
+        
+        alphas = []
+        
+        #starting input
+        word = torch.tensor(vocab.stoi['<SOS>']).view(1,-1).to(device)
+        embeds = self.embedding(word)
+
+        
+        captions = []
+        
+        for i in range(max_len):
+            alpha,context = self.attention(features, h)
+            
+            
+            #store the apla score
+            alphas.append(alpha.cpu().detach().numpy())
+            
+            lstm_input = torch.cat((embeds[:, 0], context), dim=1)
+            h, c = self.lstm_cell(lstm_input, (h, c))
+            output = self.fcn(self.drop(h))
+            output = output.view(batch_size,-1)
+        
+            
+            #select the word with most val
+            predicted_word_idx = output.argmax(dim=1)
+            
+            #save the generated word
+            captions.append(predicted_word_idx.item())
+            
+            #end if <EOS detected>
+            if vocab.itos[predicted_word_idx.item()] == "<EOS>":
+                break
+            
+            #send generated word as the next caption
+            embeds = self.embedding(predicted_word_idx.unsqueeze(0))
+        
+        #covert the vocab idx to words and return sentence
+        return [vocab.itos[idx] for idx in captions],alphas
+    
+    
+    def init_hidden_state(self, encoder_out):
+        mean_encoder_out = encoder_out.mean(dim=1)
+        h = self.init_h(mean_encoder_out)  # (batch_size, decoder_dim)
+        c = self.init_c(mean_encoder_out)
+        return h, c
